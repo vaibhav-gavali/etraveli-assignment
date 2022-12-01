0ce90aabaaff3095f6fa07bacd30e5bc74abc34c
Vaibhav Gavali <vaibhavgavali92@gmail.com>
2022-12-01 14:05:49 +0530

    Basic components layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import HeaderComponent from './containers/HeaderComponent';
+import MovieListComponent from './containers/MovieListComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderComponent />
+      <MovieListComponent />
     </div>
   );
 }
